923ed29aeb4d54446a4fb7e9287e05aa9efcde76
Mario Dujić <mario.dujic@gmail.com>
2022-12-10 08:14:45 +0100

    Enable toggle between weather and locations screen

--- src/main.js
+++ src/main.js
@@ -2,77 +2,108 @@ const {invoke} = window.__TAURI__.tauri;
 
 let degreesRowElement;
 let todayElement;
-let dateElement;
-let timeElement;
-let cityElement;
 let locationsElement;
 let queryElement;
 let resultsElement;
 
-async function get_locations(location) {
+async function getLocations(location) {
     return await invoke("get_locations", {location});
 }
 
-async function get_degrees(lat, lon) {
-    const degrees = await invoke("get_weather", {lat, lon});
-    degrees.forEach((value, i) => {
+async function getWeather(lat, lon) {
+    return await invoke("get_weather", {lat, lon});
+}
 
-        const temperature = value["temperature"]
-        const symbol = value["icon"]
-        const dayDate = new Date(value["time"])
+async function populateWeatherScreen(lat, lon, location) {
+    const weather = await getWeather(lat, lon)
 
-        // Day container
-        const dayContainer = document.createElement("div")
-        dayContainer.className = "column padding-right"
+    const dateElement = document.createElement("div")
+    populateDateElement()
+    addTimeElement()
+    addLocationElement(location)
 
-        // Weather icon
-        const iconElement = document.createElement("img")
-        iconElement.src = `/assets/${symbol}.svg`
-        iconElement.className = 'weather-icon'
-        dayContainer.appendChild(iconElement)
+    function populateDateElement() {
+        dateElement.id = "date"
+        dateElement.className = "date"
+        todayElement.append(dateElement)
+    }
 
-        // Weather degrees
-        const degreesElement = document.createElement("p")
-        degreesElement.textContent = `${temperature}°`
-        degreesElement.className = "degree-text"
-        dayContainer.append(degreesElement)
-
-        if (i === 0) {
-            todayElement.append(dayContainer)
-
-            const dayDate = new Date(value["time"])
-            dateElement.textContent = dayDate.toLocaleString(undefined, {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                weekday: "long"
-            })
-            setInterval(runCurrentTime, 1000)
-        } else {
-            const dayElement = document.createElement("p")
-            dayElement.textContent = dayDate.toLocaleString(undefined, {
-                weekday: "long"
-            })
-            dayElement.className = "degree-text"
-            dayContainer.prepend(dayElement)
-            degreesRowElement.append(dayContainer)
-        }
+    weather.forEach((weather, index) => {
+        addDayElements(weather, index, dateElement)
     })
 }
 
-function runCurrentTime() {
+function addTimeElement() {
+    const timeElement = document.createElement("p")
+    timeElement.id = "time"
+    timeElement.className = "time"
     const currentDate = new Date()
-    timeElement.textContent = currentDate.toLocaleString(undefined, {
-        hour: '2-digit',
-        hour12: false,
-        minute: '2-digit',
-        second: '2-digit'
-    })
+    setInterval(() => {
+        timeElement.textContent = currentDate.toLocaleString(undefined, {
+            hour: '2-digit',
+            hour12: false,
+            minute: '2-digit',
+            second: '2-digit'
+        })
+    }, 1000)
+    todayElement.append(timeElement)
 }
 
-const debounce = function(fn, millis) {
+function addLocationElement(location) {
+    const locationsElement = document.createElement("p")
+    locationsElement.id = "location"
+    locationsElement.className = "location"
+    locationsElement.textContent = location
+    locationsElement.addEventListener('click', showLocationsScreen)
+    todayElement.append(locationsElement)
+}
+
+function addDayElements(weather, index, dateElement) {
+
+    const temperature = weather["temperature"]
+    const symbol = weather["icon"]
+    const dayDate = new Date(weather["time"])
+
+    // Day container
+    const dayContainer = document.createElement("div")
+    dayContainer.className = "column padding-right"
+
+    // Weather icon
+    const iconElement = document.createElement("img")
+    iconElement.src = `/assets/${symbol}.svg`
+    iconElement.className = 'weather-icon'
+    dayContainer.appendChild(iconElement)
+
+    // Weather degrees
+    const temperatureElement = document.createElement("p")
+    temperatureElement.textContent = `${temperature}°`
+    temperatureElement.className = "temperature"
+    dayContainer.append(temperatureElement)
+
+    if (index === 0) {
+        todayElement.append(dayContainer)
+
+        const dayDate = new Date(weather["time"])
+        dateElement.textContent = dayDate.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            weekday: "long"
+        })
+    } else {
+        const dayElement = document.createElement("p")
+        dayElement.textContent = dayDate.toLocaleString(undefined, {
+            weekday: "long"
+        })
+        dayElement.className = "temperature"
+        dayContainer.prepend(dayElement)
+        degreesRowElement.append(dayContainer)
+    }
+}
+
+const debounce = function (fn, millis) {
     let timer;
-    return function() {
+    return function () {
         let context = this;
         let args = arguments;
         clearTimeout(timer);
@@ -82,19 +113,38 @@ const debounce = function(fn, millis) {
     }
 }
 
-function findLocation() {
-    get_locations(queryElement.value).then(locations => {
+function showLocationsScreen() {
+    locationsElement.style.display = "block"
+    clearWeatherContent()
+}
+
+function clearWeatherContent() {
+    todayElement.replaceChildren()
+    degreesRowElement.replaceChildren()
+}
+
+function showWeatherScreen() {
+    locationsElement.style.display = "none"
+    clearLocationsContent()
+}
+
+function clearLocationsContent() {
+    queryElement.value = ""
+    resultsElement.replaceChildren()
+}
+
+function searchLocation() {
+    getLocations(queryElement.value).then(locations => {
         resultsElement.replaceChildren()
         locations.forEach(location => {
             const resultElement = document.createElement("p")
             resultElement.textContent = `${location["name"]}, ${location["admin1"]}, ${location["country"]}`
             resultElement.addEventListener('click', function () {
                 const name = location["name"]
-                cityElement.textContent = name
                 const lat = parseFloat(location["lat"])
                 const lon = parseFloat(location["lon"])
-                get_degrees(lat, lon)
-                    .then(() => {locationsElement.style.display = "none"})
+                populateWeatherScreen(lat, lon, name)
+                    .then(() => showWeatherScreen())
                     .catch(reason => alert(reason))
             })
             resultsElement.appendChild(resultElement)
@@ -105,12 +155,9 @@ function findLocation() {
 window.addEventListener("DOMContentLoaded", () => {
     degreesRowElement = document.querySelector("#timeline");
     todayElement = document.querySelector("#today");
-    dateElement = document.querySelector("#date");
-    timeElement = document.querySelector("#time");
-    cityElement = document.querySelector("#city");
     locationsElement = document.querySelector("#locations")
     queryElement = document.querySelector("#query")
     resultsElement = document.querySelector("#results")
 
-    queryElement.addEventListener('input', debounce(findLocation, 500));
+    queryElement.addEventListener('input', debounce(searchLocation, 500));
 });
